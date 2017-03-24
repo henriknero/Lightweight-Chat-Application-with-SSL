3c63b6c20baebdbaa567548eedcaec4d7a061402
henriknero <henriknero@gmail.com>
2017-03-24 21:58:35 +0100

    Update MainActivity.java
    Updatering 1

--- android/MainActivity.java
+++ android/MainActivity.java
@@ -1,6 +1,7 @@
-package com.henriknero.chatapplication;
+package com.henriknero.chatapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -9,30 +10,31 @@ import android.widget.EditText;
 import android.widget.ListView;
 
 import java.io.IOException;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
-import java.net.Socket;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        try {
-            socketClient();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
+        //Skapa lista och lägg in den in listadaptern
         final List<ChatMessage> listItems = new ArrayList<>();
-        final ListView chatWindow = (ListView)findViewById(R.id.chatWindow);
         final MyListAdapter adapter = new MyListAdapter(this, listItems);
+
+        //Koppla listView i activity_main till variabeln ListView och applicera adaptern
+        final ListView chatWindow = (ListView)findViewById(R.id.chatWindow);
         chatWindow.setAdapter(adapter);
+
+        //Create EditText and Button variable and connect it to the editText and sendButton in activity_main
         final EditText editText = (EditText)findViewById(R.id.editText);
         Button sendButton = (Button)findViewById(R.id.sendButton);
 
+        //Create sendButton Listener that sends messages.
         sendButton.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
@@ -41,19 +43,28 @@ public class MainActivity extends AppCompatActivity {
                         if(!tempString.isEmpty()) {
                             adapter.addListItem(new ChatMessage(tempString, "Henrik Nero"));
                             editText.setText("");
+
                         }
                     }
                 }
         );
-
     }
-    private void socketClient() throws IOException {
-        Socket test = new Socket("192.168.1.218", 6789);
-        test.close();
+    class Thread extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... params){
+            String temp = "";
+            try {
+                Socket socket = new Socket("10.0.2.2", 6789);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return temp;
+        }
+        @Override
+        protected void onPostExecute(String result){
+            EditText editText = (EditText)findViewById(R.id.editText);
+            editText.setText(result);
+        }
     }
 }
-/**
- * Links that were used during process
- * -----------------------------------
- *https://www.youtube.com/watch?v=Xn0tQHpMDnM&t=1532s
- */
